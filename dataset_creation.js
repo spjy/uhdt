ac9722faab76dd99830244a585e8300ae6e1788e
spjy <spjy@hawaii.edu>
2019-01-06 20:29:44 -1000

    feat: Automatic tfrecord generation and minor fixes

--- dataset_creation.js
+++ dataset_creation.js
@@ -1,89 +1,91 @@
 const Jimp = require('jimp');
 const fs = require('fs');
 const { sep } = require('path');
+const exec = require('child_process').exec;
 
-const imageDirectory = `.${sep}dataset${sep}images${sep}test`;
+const imageDirectory = `.${sep}dataset${sep}images${sep}tfr`;
 
 // Generate random number with max and min values
 const rand = (max, min) => Math.floor((Math.random() * max) + min);
+
+// Define possible alphanumeric values
+const letters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZ';
+
+// Define possible shapes
+const shapes = [
+  'circle',
+  'semicircle',
+  'quarter_circle',
+  'triangle',
+  'square',
+  'rectangle',
+  'trapezoid',
+  'pentagon',
+  'hexagon',
+  'heptagon',
+  'octagon',
+  'star',
+  'cross',
+];
+
+// Red, green, blue, black, white, grey, yellow, purple, brown, orange
+const colors = [
+  [
+    { apply: 'red', params: [255] },
+    { apply: 'green', params: [0] },
+    { apply: 'blue', params: [0] }
+  ],
+  [
+    { apply: 'red', params: [0] },
+    { apply: 'green', params: [255] },
+    { apply: 'blue', params: [0] }
+  ],
+  [
+    { apply: 'red', params: [0] },
+    { apply: 'green', params: [0] },
+    { apply: 'blue', params: [255] }
+  ],
+  [
+    { apply: 'red', params: [128] },
+    { apply: 'green', params: [0] },
+    { apply: 'blue', params: [128] }
+  ],
+  [
+    { apply: 'red', params: [165] },
+    { apply: 'green', params: [42] },
+    { apply: 'blue', params: [42] }
+  ],
+  [
+    { apply: 'red', params: [255] },
+    { apply: 'green', params: [165] },
+    { apply: 'blue', params: [0] }
+  ],
+  [
+    { apply: 'red', params: [0] },
+    { apply: 'green', params: [0] },
+    { apply: 'blue', params: [0] }
+  ],
+  [
+    { apply: 'red', params: [255] },
+    { apply: 'green', params: [255] },
+    { apply: 'blue', params: [255] }
+  ],
+  [
+    { apply: 'red', params: [128] },
+    { apply: 'green', params: [128] },
+    { apply: 'blue', params: [128] }
+  ],
+  [
+    { apply: 'red', params: [255] },
+    { apply: 'green', params: [255] },
+    { apply: 'blue', params: [0] }
+  ],
+];
 
 // Loop through images
 fs.readdirSync(imageDirectory)
   .map(async (imageName) => {
     try {
-      // Define possible alphanumeric values
-      const letters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZ';
-
-      // Define possible shapes
-      const shapes = [
-        // 'HalfCircle',
-        // 'Heptagon',
-        // 'Hexagon',
-        // 'Octagon',
-        // 'Pentagon',
-        // 'QuarterCircle',
-        // 'Rectangle',
-        'Square',
-        'Star',
-        // 'Trapezoid',
-        // 'Triangle',
-        // 'Circle'
-      ];
-
-      // Red, green, blue, black, white, grey, yellow, purple, brown, orange
-      const colors = [
-        [
-          { apply: 'red', params: [255] },
-          { apply: 'green', params: [0] },
-          { apply: 'blue', params: [0] }
-        ],
-        [
-          { apply: 'red', params: [0] },
-          { apply: 'green', params: [255] },
-          { apply: 'blue', params: [0] }
-        ],
-        [
-          { apply: 'red', params: [0] },
-          { apply: 'green', params: [0] },
-          { apply: 'blue', params: [255] }
-        ],
-        [
-          { apply: 'red', params: [128] },
-          { apply: 'green', params: [0] },
-          { apply: 'blue', params: [128] }
-        ],
-        [
-          { apply: 'red', params: [165] },
-          { apply: 'green', params: [42] },
-          { apply: 'blue', params: [42] }
-        ],
-        [
-          { apply: 'red', params: [255] },
-          { apply: 'green', params: [165] },
-          { apply: 'blue', params: [0] }
-        ],
-        [
-          { apply: 'red', params: [0] },
-          { apply: 'green', params: [0] },
-          { apply: 'blue', params: [0] }
-        ],
-        [
-          { apply: 'red', params: [255] },
-          { apply: 'green', params: [255] },
-          { apply: 'blue', params: [255] }
-        ],
-        [
-          { apply: 'red', params: [128] },
-          { apply: 'green', params: [128] },
-          { apply: 'blue', params: [128] }
-        ],
-        [
-          { apply: 'red', params: [255] },
-          { apply: 'green', params: [255] },
-          { apply: 'blue', params: [0] }
-        ],
-      ];
-
       const randLetter = rand(letters.length, 0);
       const randShape = rand(shapes.length, 0);
       const randColor = rand(colors.length, 0);
@@ -99,29 +101,34 @@ fs.readdirSync(imageDirectory)
         font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
       }
 
-      const randScaleToFit = rand(70, 50);
+      // Random shape size
+      const randDimensions = rand(70, 50);
+      const randRotation = rand(360, 0);
 
       // Attach a color and letter to the shape image
       shape
-        .gaussian(rand(6,1))
+        .gaussian(rand(2,1))
         .color(colors[randColor])
         .print(font, shape.bitmap.width / 2, shape.bitmap.height / 2, letters[randLetter])
-        .rotate(rand(360, 0))
-        .scaleToFit(randScaleToFit, randScaleToFit);
+        .rotate(randRotation)
+        .scaleToFit(randDimensions, randDimensions);
   
       // Import image into Jimp
       const image = await Jimp.read(imageDirectory + sep + imageName);
       console.log(`Loaded ${imageName}`)
   
-      // Random x and y values within the 0 to width and height of image
-      const x = rand(image.bitmap.width, 1);
-      const y = rand(image.bitmap.height, 1);
+      // Random x and y values within the width and height of image minus dimensions of 
+      const x = rand(image.bitmap.width, 1) - (randDimensions + 10);
+      const y = rand(image.bitmap.height, 1) - (randDimensions + 10);
   
       // Append shape to image
-      return image
-        .composite(shape, x - (randScaleToFit + 10), y - (randScaleToFit + 10)) // Append shape, prevent from going outside of image
+      image
+        .composite(shape, x, y) // Append shape, prevent from going outside of image
         .write(`${imageDirectory}/output/${imageName}_with_target1.jpg`);
 
+      exec(`py C:/Websites/uhdt/tfrecord_gen.py --height ${image.bitmap.height} --width ${image.bitmap.width} --filename ${imageName}_with_target1.jpg --image_path ${__dirname + '/output'} --xmins ${x} --xmaxs ${x + randDimensions} --ymins ${y} --ymaxs ${x + randDimensions} --classes_text ${shapes[randShape]} --classes ${randShape}`)
+
+      console.log(`py C:/Websites/uhdt/tfrecord_gen.py --height ${image.bitmap.height} --width ${image.bitmap.width} --filename ${imageName}_with_target1.jpg --image_path ${__dirname + '/output'} --xmins ${x} --xmaxs ${x + randDimensions} --ymins ${y} --ymaxs ${x + randDimensions} --classes_text ${shapes[randShape]} --classes ${randShape}`)
     } catch (error) {
       console.log(error);
     }
--- tfrecord_gen.py
+++ tfrecord_gen.py
@@ -1,50 +1,83 @@
 import argparse
+import numpy as np
 import tensorflow as tf
+from PIL import Image
 from object_detection.utils import dataset_util
 
+# Get command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--height', help="Height of image")
-parser.add_argument('--width', help="Width of image")
-parser.add_argument('--filename', help="Filename of image")
-parser.add_argument('--encoded_image_data', help="Encoded image data of image")
-parser.add_argument('--image_format', help="Image format of image")
-parser.add_argument('--xmins', help="xmins of image")
-parser.add_argument('--xmaxs', help="xmaxs of image")
-parser.add_argument('--ymins', help="ymins of image")
-parser.add_argument('--ymaxs', help="ymaxs of image")
-parser.add_argument('--classes_text', help="classes_text of image")
-parser.add_argument('--classes', help="classes of image")
+parser.add_argument('--height', help="Height of image", type=int)
+parser.add_argument('--width', help="Width of image", type=int)
+parser.add_argument('--filename', help="Filename of image", type=str)
+parser.add_argument('--image_path', help="Path of image", type=str)
+parser.add_argument('--xmins', help="xmins of image", type=int, nargs='+')
+parser.add_argument('--xmaxs', help="xmaxs of image", type=int, nargs='+')
+parser.add_argument('--ymins', help="ymins of image", type=int, nargs='+')
+parser.add_argument('--ymaxs', help="ymaxs of image", type=int, nargs='+')
+parser.add_argument('--classes_text', help="classes_text of image", type=str, nargs='+')
+parser.add_argument('--classes', help="classes of image", type=int, nargs='+')
+# Parse the arguments
+args = parser.parse_args()
 
-def create_tf_example(label_and_data_info):
-  # TODO START: Populate the following variables from your example.
-  height = None # Image height
-  width = None # Image width
-  filename = None # Filename of the image. Empty if image is not from file
-  encoded_image_data = None # Encoded image bytes
-  image_format = None # b'jpeg' or b'png'
+class_dict = {
+  1: b'circle',
+  2: b'semicircle',
+  3: b'quarter_circle',
+  4: b'triangle',
+  5: b'square',
+  6: b'rectangle',
+  7: b'trapezoid',
+  8: b'pentagon',
+  9: b'hexagon',
+  10: b'heptagon',
+  11: b'octagon',
+  12: b'star',
+  13: b'cross'
+}
 
-  xmins = [] # List of normalized left x coordinates in bounding box (1 per box)
-  xmaxs = [] # List of normalized right x coordinates in bounding box
-            # (1 per box)
-  ymins = [] # List of normalized top y coordinates in bounding box (1 per box)
-  ymaxs = [] # List of normalized bottom y coordinates in bounding box
-            # (1 per box)
-  classes_text = [] # List of string class name of bounding box (1 per box)
-  classes = [] # List of integer class id of bounding box (1 per box)
-  # TODO END
-  tf_label_and_data = tf.train.Example(features=tf.train.Features(feature={
-      'image/height': dataset_util.int64_feature(height),
-      'image/width': dataset_util.int64_feature(width),
-      'image/filename': dataset_util.bytes_feature(filename),
-      'image/source_id': dataset_util.bytes_feature(filename),
-      'image/encoded': dataset_util.bytes_feature(encoded_image_data),
-      'image/format': dataset_util.bytes_feature(image_format),
-      'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
-      'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
-      'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
-      'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
-      'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-      'image/object/class/label': dataset_util.int64_list_feature(classes),
-  }))
-  
-  return tf_label_and_data
+# Write tfrecord
+writer = tf.io.TFRecordWriter('C:/Websites/uhdt/tfrecords/data.tfrecord')
+
+height = args.height # Image height
+width = args.width # Image width
+image_path = args.image_path
+filename = str.encode(args.filename) # Filename of the image. Empty if image is not from file
+with tf.gfile.GFile(filename, 'rb') as fid: # Encoded image bytes
+  encoded_image_data = bytes(fid.read())
+image_format = b'jpg' # b'jpeg' or b'png'
+
+xmins = args.xmins # List of normalized left x coordinates in bounding box (1 per box)
+xmaxs = args.xmaxs # List of normalized right x coordinates in bounding box
+          # (1 per box)
+ymins = args.ymins # List of normalized top y coordinates in bounding box (1 per box)
+ymaxs = args.ymaxs # List of normalized bottom y coordinates in bounding box
+          # (1 per box)
+
+classes_text = []
+
+for text in args.classes_text:
+  t = str.encode(text)
+  classes_text.append(t)
+
+# classes_text = bytearray(args.classes_text) # List of string class name of bounding box (1 per box)
+classes = args.classes # List of integer class id of bounding box (1 per box)
+# TODO END
+tf_label_and_data = tf.train.Example(features=tf.train.Features(feature={
+    'image/height': dataset_util.int64_feature(height),
+    'image/width': dataset_util.int64_feature(width),
+    'image/filename': dataset_util.bytes_feature(filename),
+    'image/source_id': dataset_util.bytes_feature(filename),
+    'image/encoded': dataset_util.bytes_feature(encoded_image_data),
+    'image/format': dataset_util.bytes_feature(image_format),
+    'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+    'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+    'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+    'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+    'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+    'image/object/class/label': dataset_util.int64_list_feature(classes),
+}))
+
+# Write to file
+writer.write(tf_label_and_data.SerializeToString())
+
+writer.close()
